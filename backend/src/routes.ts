2483fd767194217d0291c2262025939046ac96d9
Borges10002 <borges10002@gmail.com>
2023-03-12 00:02:18 -0300

    class video four

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import { SendMailController } from "./controllers/SendMailController";
 import { SurveyController } from "./controllers/SurveyController";
 import { UserController } from "./controllers/UserController";
 
@@ -6,10 +7,13 @@ const router = Router();
 
 const userController = new UserController();
 const surveysController = new SurveyController();
+const sendMailController = new SendMailController();
 
 router.post("/users", userController.create);
 
 router.post("/surveys", surveysController.create);
 router.get("/surveys", surveysController.show);
+
+router.post("/sendMail", sendMailController.execute);
 
 export { router };
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,7 +1,7 @@
 import { app } from "./app";
 
 const port = {
-  port: 3333,
+  port: 3334,
   host: "0.0.0.0",
 };
 
